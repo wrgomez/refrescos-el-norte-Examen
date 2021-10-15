1c4d6148001d6c0a648847db0220c401d013ab19
wrgomez <91978207+wrgomez@users.noreply.github.com>
2021-10-14 18:47:57 -0600

    Add files via upload

---
+++ js/validar.js
@@ -0,0 +1,14 @@
+
+function go(){
+    if (document.formingreso.usuario.value=='arlette' && document.formingreso.contraseña.value=='examen')
+    {
+    window.location="html/inicio.html"
+    }
+
+    else
+    {
+        alert("ingrese usuario y contraseña correcta")
+    }
+
+
+}
